a460983b2ce12f23047591f100c9af1595436027
Angel Wells <angelhudgins0401@gmail.com>
2024-04-23 21:38:24 -0500

    got templates to show up finally

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'detail.html', {'product': product}) 
